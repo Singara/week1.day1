77809e528b417d23df83cd0ba87cc047572b7ab5
Singara <98253836+Singara@users.noreply.github.com>
2022-01-23 13:30:41 +0530

    Add files via upload

---
+++ CarDetails.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class CarDetails {
+	
+	byte regnNumber = 0022;
+	short towingCapacityWeight = 10000;
+	int ownerNumber = 866722592;
+	long chassisNumber = 6587231425L;
+	float compressionRatio = 10.5F;
+	double engineCapacity = 60.9;
+	char varient = 'R';
+	boolean petrolFuelType = true;
+	String Name = "KiaCeed";
+	
+
+}
